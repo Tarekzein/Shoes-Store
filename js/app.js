b091b548404659741ec43d35279cc4338b151d9c
Tarek Zein <tarekzein800@gmail.com>
2022-07-05 09:52:29 +0200

    new sections

---
+++ js/app.js
@@ -0,0 +1,74 @@
+// menu icon change
+
+let menuIcon = document.querySelector(".menu-icon");
+let xIcon = document.querySelector(".fa-xmark");
+
+menuIcon.addEventListener("click", () => {
+
+  let menuVertical = document.querySelector(".vertical-menu");
+  
+    menuVertical.style.right = "0";
+    menuIcon.style.position = "fixed";
+  
+  
+});
+
+xIcon.addEventListener("click", () => {
+  let menuVertical = document.querySelector(".vertical-menu");
+  menuVertical.style.right = "-1000px";
+  menuIcon.style.position = "absolute";
+})
+
+
+// t-shirts slideshow
+
+var shirtIndex = 1;
+showTshirt(shirtIndex);
+
+function plusDivs(n) {
+  showTshirt(shirtIndex += n);
+}
+
+function showTshirt(n) {
+  var i;
+  var shirts = document.getElementsByClassName("shirt");
+  let shape = document.querySelectorAll(".shapes div");
+  let price = document.querySelector("#price span");
+  if (n > shirts.length) {
+    shirtIndex = 1
+  }
+  if (n < 1) {
+    shirtIndex = shirts.length;
+  }
+
+  for (i = 0; i < shirts.length; i++) {
+    shirts[i].style.display = "none";
+    shape[i].classList.remove("active");
+  }
+  shirts[shirtIndex-1].style.display = "block";
+  shape[shirtIndex - 1].classList.add("active");
+  price.innerHTML = `${shirts[shirtIndex - 1].dataset.price}`;
+}
+
+// pageUp button
+
+let pageBtn = document.querySelector(".page-up");
+pageBtn.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+})
+
+function pageBtnStat() {
+if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+  pageBtn.style.display = "flex";
+}
+else {
+  pageBtn.style.display = "none";
+  
+  }
+}
+
+
+// on scrolling 
+window.onscroll = () => {
+  pageBtnStat();
+}
